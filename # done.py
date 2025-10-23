ab3d8bd800973ad24a95e6ed920bb854de041dfe
EXVLO <nadirashvilisandro888@gmail.com>
2025-10-23 13:43:50 +0700

    added f-c c-f

--- # done.py
+++ # done.py
@@ -3,3 +3,13 @@
 print("Hello, World")
 print("GUGA")
 print("GUGU")
+
+# Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+cels = float(input("Enter temperature in Celsius: "))
+faren = cels * (9 / 5) + 32
+print(faren)
+
+# Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9.
+faren2 = float(input("Enter temperature in Fahrenheit: "))
+cels2 = (faren2 - 32) * 5 / 9
+print(cels2)
