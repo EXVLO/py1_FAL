9f7e0d377155ac18516f55d7b8de253d51b1f147
EXVLO <nadirashvilisandro888@gmail.com>
2025-10-23 13:54:20 +0700

    added rounds

--- # done.py
+++ # done.py
@@ -7,9 +7,9 @@ print("GUGU")
 # Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
 cels = float(input("Enter temperature in Celsius: "))
 faren = cels * (9 / 5) + 32
-print(faren)
+print(round(faren, 2))
 
 # Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9.
 faren2 = float(input("Enter temperature in Fahrenheit: "))
 cels2 = (faren2 - 32) * 5 / 9
-print(cels2)
+print(round(cels2, 2))
